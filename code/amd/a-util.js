20afec53af2fc32315da1155f25d2d53f0114040
lijingke <1431806069@qq.com>
2019-08-01 14:19:55 +0800

    模块化-AMD

---
+++ code/amd/a-util.js
@@ -0,0 +1,16 @@
+define(['./util.js'], function(util) {
+    var aUtil = {
+        aGetFormatDate: function(date) {
+            return util.getFormatDate(date, 2)
+        }
+    }
+    return aUtil
+})
+
+// define(function() {
+//   return {
+//       aGetFormatDate: function() {
+//           return 'abc'
+//       }
+//   }    
+// })
