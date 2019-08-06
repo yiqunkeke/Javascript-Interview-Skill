2a8c165a0ef26839f810574edca4bb8b74a29be3
lijingke <1431806069@qq.com>
2019-08-06 09:05:34 +0800

    构建工具-使用jquery-演示commonJS模块化-require引入第三方模块和自定义模块的使用区别

--- code/webpack/src/app.js
+++ code/webpack/src/app.js
@@ -1,1 +1,11 @@
-console.log(100)
+// console.log(100)
+
+// 1. require 使用package.json中的第三方模块，参数为模块名
+// var $ = require('jquery')
+// console.log($)
+// var $root = $('#root')
+// $root.html('<p>这是 jquery插入的文字</p>')
+
+// 2. require 使用自定义模块，参数为路径
+var aUtil = require('./a-util')
+aUtil.print()
